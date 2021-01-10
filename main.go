94a12b17ca4c07821a89a18e87327905720c4edd
Sami2077 <fahimalsafi@gmail.com>
2021-01-10 17:02:30 -0500

    additional function

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ import (
 )
 
 var (
-	MongoURI   = flag.String("mongouri", "mongodb://localhost:27017", "test_database")
-	collection = flag.String("CRUD Database", "test", " for example")
+	MongoURI   = flag.String("mongouri", "mongodb+srv://sami_92:sami_92@cluster0.dovo5.mongodb.net/?retryWrites=true&w=majority", "test_database")
+	collection = flag.String("CRUD Database", "maxcrm", " for example")
 	DBClient   *mongo.Client
 	AppDB      *mongo.Database
 )
@@ -62,14 +62,29 @@ func init() {
 
 func main() {
 	r := mux.NewRouter()
+	r.Methods("OPTIONS").HandlerFunc(HandleOptions)
 	r.HandleFunc("/", Hello).Methods("GET")
 	r.HandleFunc("/register", Register).Methods("POST")
 	r.HandleFunc("/login", Login).Methods("POST")
 	r.HandleFunc("/auth/{token}", Auth).Methods("GET")
+	r.HandleFunc("/kill/{username}", DeleteSession).Methods("GET")
 
 	log.Info("Server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 
+}
+
+func HandleOptions(w http.ResponseWriter, r *http.Request) {
+	CORSHeaders(w, r)
+}
+
+func CORSHeaders(w http.ResponseWriter, r *http.Request) {
+	headers := w.Header()
+	log.Infof("Request Origin is: %v", r.Header.Get("Origin"))
+	headers.Add("Access-Control-Allow-Origin", "*")
+	headers.Add("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token, api-key")
+	headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	headers.Add("Access-Control-Max-Age", "3600")
 }
 
 func DBConnect(URI string) *mongo.Client {
@@ -90,11 +105,13 @@ func DBConnect(URI string) *mongo.Client {
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
+	CORSHeaders(w, r)
 	fmt.Fprintf(w, "Homepage")
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	CORSHeaders(w, r)
+	//w.Header().Set("Content-Type", "application/json")
 	var user User
 	body, _ := ioutil.ReadAll(r.Body)
 	err := json.Unmarshal(body, &user)
@@ -141,7 +158,8 @@ func Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	CORSHeaders(w, r)
+	//w.Header().Set("Content-Type", "application/json")
 	var user User
 	body, _ := ioutil.ReadAll(r.Body)
 	err := json.Unmarshal(body, &user)
@@ -209,7 +227,8 @@ func Login(w http.ResponseWriter, r *http.Request) {
 }
 
 func Auth(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	CORSHeaders(w, r)
+	//w.Header().Set("Content-Type", "application/json")
 	var response ResponseResult
 	var user User
 	token := mux.Vars(r)["token"]
@@ -225,11 +244,13 @@ func Auth(w http.ResponseWriter, r *http.Request) {
 
 	log.Info("Token Found")
 
-	normalTime := time.Unix(user.TokenExpired, 0)
-	addOneHour := normalTime.Add(1 * time.Hour)
+	//normalTime := time.Unix(user.TokenExpired, 0)
+	addOneHour := time.Now().Add(1 * time.Hour)
 	user.TokenExpired = addOneHour.Unix()
 
-	up, err := collection.UpdateOne(context.TODO(), bson.M{"token": token}, bson.D{{Key: "$set", Value: user}})
+	up, err := collection.UpdateOne(context.TODO(),
+		bson.M{"token": token},
+		bson.D{{Key: "$set", Value: user}})
 	if err != nil {
 		log.Info("Error extending Session")
 		response.Data = "User not Found"
@@ -240,7 +261,7 @@ func Auth(w http.ResponseWriter, r *http.Request) {
 	log.Info("Session Extended ", up.ModifiedCount)
 	var responseUser ResponseUser
 
-	responseUser.Expires = normalTime
+	responseUser.Expires = time.Unix(user.TokenExpired, 0)
 	responseUser.SessionToken = user.Token
 
 	responseUser.UserInfo = UserInfo{
@@ -252,4 +273,28 @@ func Auth(w http.ResponseWriter, r *http.Request) {
 	response.Data = responseUser
 	json.NewEncoder(w).Encode(response)
 	return
+}
+
+func DeleteSession(w http.ResponseWriter, r *http.Request) {
+	CORSHeaders(w, r)
+	var response ResponseResult
+	username := mux.Vars(r)["username"]
+	collection := AppDB.Collection("crmuser")
+	delete := collection.FindOneAndUpdate(context.TODO(),
+		bson.M{"username": username},
+		bson.D{
+			{"$set", bson.D{{"token", ""}}},
+		},
+	)
+	if delete == nil {
+		log.Info("Error deleting Token")
+		response.Error = "Error deleting Token"
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	response.Status = "ok"
+	json.NewEncoder(w).Encode(response)
+	return
+
 }
